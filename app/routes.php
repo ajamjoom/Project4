6b19ae5ff8e2b21b2ab617b9bff1c77bf8811c62
Abdulrahman Jamjoom <abdulrahmanjamjoom15@kingsacademy.edu.jo>
2014-07-25 18:53:50 -0400

    placed all logic in controllers and locked all pages and fixes the redirect accordingly

--- app/routes.php
+++ app/routes.php
@@ -11,85 +11,20 @@
 |
 */
 
-Route::get('/', 'MainController@showhomepage');
+Route::get('/', array( 'before' => 'auth','uses'=> 'MainController@showhomepage'));
 
-Route::get('/add_question', 'MainController@showaddquestion');
+Route::get('/add_question', array( 'before' => 'auth','uses'=> 'MainController@showaddquestion'));
 
-Route::get('/view_all_questions', 'MainController@showallquestions');
+Route::get('/view_all_questions', array( 'before' => 'auth','uses'=> 'MainController@showallquestions'));
 
-Route::get('/view_all_my_forums', 'MainController@showallmyforums');
+Route::get('/view_all_my_forums', array( 'before' => 'auth','uses'=> 'MainController@showallmyforums'));
 
-Route::get('/signup',
-    array(
-        'before' => 'guest',
-        function() {
-            return View::make('signup');
-        }
-    )
-);
+Route::get('/signup', array( 'before' => 'guest', 'uses' => 'AuthenticationController@showsignup'));
 
-Route::post('/signup', 
-    array(
-        'before' => 'csrf', 
-        function() {
+Route::post('/signup', array('before' => 'csrf', 'uses' => 'AuthenticationController@proccesssignup'));
 
-            $user = new User;
-            $user->username = Input::get('username');
-            $user->email    = Input::get('email');
-            $user->password = Hash::make(Input::get('password'));
+Route::get('/login', array( 'before' => 'guest', 'uses' => 'AuthenticationController@showlogin'));
 
-            # Try to add the user 
-            try {
-                $user->save();
-            }
-            # Fail
-            catch (Exception $e) {
-                return Redirect::to('/signup')->with('flash_message', 'Sign up failed; please try again.')->withInput();
-            }
+Route::post('/login', array('before' => 'csrf', 'uses' => 'AuthenticationController@proccesslogin'));
 
-            # Log the user in
-            Auth::login($user);
-
-            return Redirect::to('/')->with('flash_message', 'Welcome to What??');
-
-        }
-    )
-);
-
-Route::get('/login',
-    array(
-        'before' => 'guest',
-        function() {
-            return View::make('login');
-        }
-    )
-);
-
-Route::post('/login', 
-    array(
-        'before' => 'csrf', 
-        function() {
-
-            $credentials = Input::only('username', 'password');
-
-            if (Auth::attempt($credentials, $remember = true)) {
-                return Redirect::intended('/')->with('flash_message', 'Welcome Back!');
-            }
-            else {
-                return Redirect::to('/login')->with('flash_message', 'Log in failed; please try again.');
-            }
-
-            return Redirect::to('login');
-        }
-    )
-);
-
-Route::get('/logout', function() {
-
-    # Log out
-    Auth::logout();
-
-    # Send them to the homepage
-    return Redirect::to('/');
-
-});
+Route::get('/logout', 'AuthenticationController@logout');
