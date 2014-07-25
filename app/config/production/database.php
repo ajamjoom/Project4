ed1510be2025cdfaf3a2551344b6e12955b5fe2a
Abdulrahman Jamjoom <abdulrahmanjamjoom15@kingsacademy.edu.jo>
2014-07-25 19:57:36 -0400

    set the database in the production mode

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'hostname-goes-here',
+            'database'  => 'what',
+            'username'  => 'jacqulyn',
+            'password'  => 'kwrPfApm',
+        ),
+
+    ),
+
+);
